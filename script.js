509a08ede0b9eaa1635986cb5c3db140a19e5ecf
Ciuff4 <gasperini_cristina@libero.it>
2022-05-04 16:22:17 +0200

    preparazione html+css+stampa text di todo

---
+++ script.js
@@ -0,0 +1,24 @@
+const app= new Vue({
+    el:'#app',
+    
+    data:{
+        todos:[
+            {
+                text:'ciao',
+                done:'false',
+            },
+            {
+                text:'ciao2',
+                done:'true',
+            },
+            {
+                text:'ciao3',
+                done:'false',
+            }
+        ]
+    },
+
+    methods:{
+        
+    }
+})
